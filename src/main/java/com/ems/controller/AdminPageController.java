f18356c0ba9eed24e68e1e54f02d8ecd6e1503eb
Vishal Soni <vso55089@gmail.com>
2026-05-06 11:27:46 +0530

    Changes in leave.

--- src/main/java/com/ems/controller/AdminPageController.java
+++ src/main/java/com/ems/controller/AdminPageController.java
@@ -165,7 +165,7 @@ public class AdminPageController {
 
        employeeLeaveService.leaveProcess(action, empId);
         
-        return "redirect:/AdminLeaveManagement";
+        return "redirect:/admin/AdminLeaveManagement";
     }
     
 
